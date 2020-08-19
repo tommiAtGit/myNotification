be128e3235520ee080b02b2e36e5cadaa7d43f85
Tommi Korteikko <tommi.korteikkocinia.fi@Tommi-MacBook-Pro.local>
2020-08-19 15:27:37 +0300

    initial commit

---
+++ src/main/java/com/myproject/example/pushnotification/myNotification/MyNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.example.pushnotification.myNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/example/pushnotification/myNotification/MyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.example.pushnotification.myNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
