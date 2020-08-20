f89e3382d056a2976ba2a6414ed72137a5d5234f
Tommi Korteikko <tommi.korteikkocinia.fi@Tommi-MacBook-Pro.local>
2020-08-20 14:37:01 +0300

    Notification functionality added

---
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    messagingSenderId: "1035053093238",
+    apiKey: "AIzaSyD6hPZ3ZYx3pvJZ2Z4Ezn4nx6cu96oTdUk",
+    projectId: "testingportaikko",
+    appId: "1:1035053093238:web:ae1c355d354535836d6a57",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
